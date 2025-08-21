ec51ab19017c7f2cf14a4a1b4ae6d0bc99c3d935
Paul <paulezumezu@gmail.com>
2025-08-21 16:02:14 +0100

    New edit for sql query

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -29,7 +29,7 @@ CREATE TABKE Orders(
 order_id INT PRIMARY KEY,
 customer_id INT,
 order_date DATE,
-FOREIGN KEY customer_id REFERENCES customers(customer_id)
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 
@@ -38,7 +38,7 @@ orderdetailid INT PRIMARY KEY,
 order_ID INT,
 book_id INT,
 quantity DOUBLE,
-FOREIGN KEY Order_id REFERENCES orders(order_id),
-FOREIGN KEY book_id REFERENCES books(book_id)
+FOREIGN KEY (Order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
