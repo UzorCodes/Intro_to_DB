d6c726afc0ec06b797ccbec8a894b6787b63f138
Paul <paulezumezu@gmail.com>
2025-08-21 17:13:09 +0100

    New changes to python script

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,3 +1,7 @@
+import from mysql.connector
+
 sql = "CREATE DATABASE IF NOT EXISTS alx_book_store;"
 
 mycursor.execute(sql)
+
+mydb = mysql.connector.connect(host="localhost", user="root", passwd="1234")
