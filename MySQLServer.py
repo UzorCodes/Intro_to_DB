fec679bcfe976059909d960b3d14fb71f865cdb9
Paul <paulezumezu@gmail.com>
2025-08-21 17:21:50 +0100

    New changes to python script

--- MySQLServer.py
+++ MySQLServer.py
@@ -3,5 +3,10 @@ import mysql.connector
 sql = "CREATE DATABASE IF NOT EXISTS alx_book_store;"
 
 mycursor.execute(sql)
+print(f"Database 'alx_book_store' created successfully!")
 
-mydb = mysql.connector.connect(host="localhost", user="root", passwd="1234")
+try:
+    mydb = mysql.connector.connect(host="localhost", user="root", passwd="1234")
+    conn.close()
+except mysql.connector.Error:
+    print(f"Error: Failed to connect to the database.")
