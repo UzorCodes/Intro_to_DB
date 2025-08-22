03c3a6d4bb6e08656b89c125d2c134c2c5becbe1
Paul <paulezumezu@gmail.com>
2025-08-22 14:18:43 +0100

    New task_6 script

---
+++ task_6.sql
@@ -0,0 +1,7 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost", user="root", passwd="1234")
+
+result = "INSERT INTO customer (customer_id, customer_name, email, address) VALUES (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."), (3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."), (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.") WHERE TABLE_SCHEMA = 'alx_book_store';"
+
+mycursor.execute(result)
