38f76a6e12287c2615095bb2112acc257e456543
Paul <paulezumezu@gmail.com>
2025-08-22 09:52:08 +0100

    New task_4 script

---
+++ task_4.sql
@@ -0,0 +1,9 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(host="localhost", user="root", passwd="1234")
+
+tabledetails = "SHOW COLUMNS FROM books;"
+
+result = mycursor.execute(tabledetails)
+
+print(f'result')
