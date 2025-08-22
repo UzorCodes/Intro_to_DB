e851724dc2c961d58a1ecf37e23c4bb8d18db806
Paul <paulezumezu@gmail.com>
2025-08-22 10:00:06 +0100

    Edit task_4 script

--- task_4.sql
+++ task_4.sql
@@ -2,7 +2,7 @@ import mysql.connector
 
 mydb = mysql.connector.connect(host="localhost", user="root", passwd="1234")
 
-tabledetails = "SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE, COLUMN_TYPE, COLUMN_DEFAULT, IS_NULLABLE, CHARACTER_MAXIMUM_LENGTH, NUMERIC_PRECISION, COLUMN_KEY, EXTRA, ORDINAL_POSITION FROM information_schema.columns WHERE ( table_schema = 'alx_book_store' AND TABLE_NAME = 'books');"
+tabledetails = "SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE, COLUMN_TYPE, COLUMN_DEFAULT, IS_NULLABLE, CHARACTER_MAXIMUM_LENGTH, NUMERIC_PRECISION, COLUMN_KEY, EXTRA, ORDINAL_POSITION FROM INFORMATION_SCHEMA.COLUMNS WHERE (TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books');"
 
 result = mycursor.execute(tabledetails)
 
